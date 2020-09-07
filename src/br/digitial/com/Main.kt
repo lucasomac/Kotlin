c5666eb53a718b894fcc83be3598a8d9558ab70d
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-09-07 18:55:09 -0300

    Main principal

--- src/br/digitial/com/Main.kt
+++ src/br/digitial/com/Main.kt
@@ -1,12 +1,9 @@
 package br.digitial.com
 
-import br.digitial.com.santander.Cliente
-import br.digitial.com.santander.Conta
-
 fun main() {
-    val c1 = Cliente("Lucas", "Macedo")
-    val c2 = Cliente("Mateus", "Macedo")
-
-    var ct1 = Conta(1, 0.0, c1)
-    var ct2 = Conta(1, 0.0, c2)
+//    val c1 = Cliente("Lucas", "Macedo")
+//    val c2 = Cliente("Mateus", "Macedo")
+//
+//    var ct1 = Conta(1, 0, c1)
+//    var ct2 = Conta(1, 0, c2)
 }
